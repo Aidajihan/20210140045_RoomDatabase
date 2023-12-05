c90ef1f3aff7492fdd2c9efb0d4d51848d0cd522
TC1045 <aida.jihan.ft21@mail.umy.ac.id>
2023-12-05 20:28:00 +0700

    Membuat dataclass DetailSiswa dengan variabel id, nama, alamat, dan telpon serta membuat function untuk mengkonversi data input ke data dalam tabel sesuai jenis datanya

--- app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
@@ -1,10 +1,13 @@
 package com.example.roomsiswa.model
 
+import android.app.AlarmManager
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.example.roomsiswa.data.Siswa
 import com.example.roomsiswa.repositori.RepositoriSiswa
+import java.util.NavigableMap
 
 class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
     /**
@@ -14,7 +17,7 @@ class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() 
         private set
 
     /* Fungsi untuk menvalidasi imput */
-    private fun validasiInput (uiState: DetaixlSiswa = uiStateSiswa.detailSiswa) : Boolean{
+    private fun validasiInput (uiState: DetailSiswa = uiStateSiswa.detailSiswa) : Boolean{
         return  with(uiState) {
             nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
         }
@@ -39,4 +42,30 @@ class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() 
 data class  UIStateSiswa(
     val  detailSiswa: DetailSiswa = DetailSiswa(),
     val  isEntryValid: Boolean = false
+)
+
+data class  DetailSiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val alamat: String = "",
+    val telpon: String = "",
+)
+/* Fungsi untuk mengkonversi data input ke data dalam tabel sesuai jenis daanya */
+fun DetailSiswa.toSiswa(): Siswa = Siswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
+
+fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
+)
+
+fun  Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
 )
