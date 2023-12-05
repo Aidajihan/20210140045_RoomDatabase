7a9be53dc4de994789def5fa355467b7fcd7bf7e
TC1045 <aida.jihan.ft21@mail.umy.ac.id>
2023-12-05 20:14:27 +0700

    Membuat Fungsi untuk menvalidasi Input di EntryViewModel.kt

---
+++ app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.roomsiswa.model
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.roomsiswa.repositori.RepositoriSiswa
+
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
+    /**
+     * Berisi status siswa saat ini
+     */
+    var  uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    /* Fungsi untuk menvalidasi imput */
+    private fun validasiInput (uiState: DetaixlSiswa = uiStateSiswa.detailSiswa) : Boolean{
+        return  with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
+    }
+}
